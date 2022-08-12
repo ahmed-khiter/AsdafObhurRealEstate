fb5b6ececa8967db80c2dcfe3a5aadfca21a90d9
ahmed khaled <ahmed.atyia@amcde.com>
2022-08-12 23:11:38 +0200

    modify design

--- AsdafObhurRealEstate/Enums/StatusOfClient.cs
+++ AsdafObhurRealEstate/Enums/StatusOfClient.cs
@@ -1,10 +1,19 @@
-﻿namespace AsdafObhurRealEstate.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsdafObhurRealEstate.Enums
 {
     public enum StatusOfClient
     {
+        [Display(Name ="جديد")]
         NewRequest = 1,
+
+        [Display(Name ="تحت الإجراء")]
         UnderProgress = 2,
+
+        [Display(Name ="انتهي")]
         Finished = 3 ,
+
+        [Display(Name = "للإدارة المالية")]
         FinancialTransformation = 4 ,
     }
 }
