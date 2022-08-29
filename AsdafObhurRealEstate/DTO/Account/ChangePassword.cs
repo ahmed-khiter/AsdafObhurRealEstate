7ea4c24da5ffd7ad449749d38ed9585f682e16b7
ahmed khaled <ahmed.atyia@amcde.com>
2022-08-29 10:15:24 +0200

    Fix rooting

---
+++ AsdafObhurRealEstate/DTO/Account/ChangePassword.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+
+namespace AsdafObhurRealEstate.DTO.Account
+{
+    public class ChangePassword
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "الرقم السري القديم")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "الرقم السري الجديد")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password), Display(Name = "تأكيد الرقم السرى"), Compare(nameof(NewPassword))]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
